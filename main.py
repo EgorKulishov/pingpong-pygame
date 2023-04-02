480b45c73b279fbba6bc7c4ab899b4081f13fad3
Егор Кулишов <naladwepo@gmail.com>
2023-04-02 18:13:10 +0400

    fix

--- main.py
+++ main.py
@@ -5,71 +5,62 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+
 class GameSprite(sprite.Sprite):
- #конструктор класса
-   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-       #вызываем конструктор класса (Sprite):
-       sprite.Sprite.__init__(self)
+    def __init__(self, player_image, player_x, player_y, player_speed,size_x, size_y):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-       #каждый спрайт должен хранить свойство image - изображение
-       self.image = transform.scale(image.load(player_image), (size_x, size_y))
-       self.speed = player_speed
-
-
-       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
- #метод, отрисовывающий героя на окне
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
-
-#класс главного игрока
 class Player(GameSprite):
-   #метод для управления спрайтом стрелками клавиатуры
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_RIGHT] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_d] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 speed_x = 3
 speed_y = 3
-
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
-
 font.init()
 
 font1 = font.Font(None, 35)
 lose1 = font1.render('PLAYER 1 LOSE!', True, (180,0,0))
 lose2 = font1.render('PLAYER 2 LOSE!', True, (180,0,0))
 
-
 racket1 = Player("src/racket.png", 30, 200, 4, 50, 150)
 racket2 = Player("src/racket.png", 520, 200, 4, 50, 150)
-ball = GameSprite("src/tenis_ball.png", 520, 200, 4, 50, 150)
+ball = GameSprite("src/tenis_ball.png", 520, 200, 4, 50, 50)
 
 while game:
     for e in event.get():
-        if e.type == quit():
+        if e.type == QUIT:
             game = False
+
     if finish != True:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
 
@@ -79,11 +70,13 @@ while game:
         if ball.rect.x < 0:
             finish = True
             window.blit(lose1, (200, 200))
-            game_over = True
+            game_over = False
+
         if ball.rect.x > win_width:
             finish = True
             window.blit(lose2, (200, 200))
-            game_over = True
+            game_over = False
+
         racket1.reset()
         racket2.reset()
         ball.reset()
