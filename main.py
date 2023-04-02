a0445a7cd5da33e108d984d45617d4fbbfa8032c
Егор Кулишов <naladwepo@gmail.com>
2023-04-02 18:05:03 +0400

    Add lose

--- main.py
+++ main.py
@@ -41,11 +41,20 @@ class Player(GameSprite):
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
 
+speed_x = 3
+speed_y = 3
 
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+font.init()
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180,0,0))
+lose1 = font1.render('PLAYER 2 LOSE!', True, (180,0,0))
+
 
 racket1 = Player("src/racket.png", 30, 200, 4, 50, 150)
 racket2 = Player("src/racket.png", 520, 200, 4, 50, 150)
@@ -55,13 +64,25 @@ while game:
     for e in event.get():
         if e.type == quit():
             game = False
-    window.fill(back)
-    racket1.update_l()
-    racket2.update_r()
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
 
-    racket1.reset()
-    racket2.reset()
-    ball.reset()
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
-    display.update()
-    clock.tick(FPS)
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+        display.update()
+        clock.tick(FPS)
