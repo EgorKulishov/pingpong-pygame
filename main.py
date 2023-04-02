7a6df3821fb9117d1c4867e48c1596558648434b
Егор Кулишов <naladwepo@gmail.com>
2023-04-02 18:20:49 +0400

    fix

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.y > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_RIGHT] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_d] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
 speed_x = 3
