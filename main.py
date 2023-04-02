a696035fe4cc1e2eaaa928ee4cd6b35cae6dc506
Егор Кулишов <naladwepo@gmail.com>
2023-04-02 17:53:29 +0400

    Add while game

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pygame import *
-win_width = 700
+
+back = (200,255,255)
+win_width = 600
 win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
 class GameSprite(sprite.Sprite):
  #конструктор класса
    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -37,6 +41,27 @@ class Player(GameSprite):
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
 
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
 racket1 = Player("src/racket.png", 30, 200, 4, 50, 150)
 racket2 = Player("src/racket.png", 520, 200, 4, 50, 150)
 ball = GameSprite("src/tenis_ball.png", 520, 200, 4, 50, 150)
+
+while game:
+    for e in event.get():
+        if e.type == quit():
+            game = False
+    window.fill(back)
+    racket1.update_l()
+    racket2.update_r()
+
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+
+    display.update()
+    clock.tick(FPS)
